c77753afed4d3e5f293ab189f77312e7d81950aa
Greatgabbar <63102346+Greatgabbar@users.noreply.github.com>
2020-04-12 16:10:13 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ window.addEventListener('keydown',function(e){
   kel.className+=' playing';
   key.currentTime=0;
    key.play();
-   this.setTimeout(function(){
-     kel.className='key';
-   },70);
+  //  this.setTimeout(function(){
+  //    kel.className='key';
+  //  },70);
 });
+
+const keys=document.querySelectorAll('.key');
+keys.forEach((key)=>{
+  key.addEventListener('transitionend',removeTransition);
+})
+
+function removeTransition(e){
+  // console.log(e)
+  if(e.propertyName!=='transform')
+  return;
+  this.classList.remove('playing');
+}
 
